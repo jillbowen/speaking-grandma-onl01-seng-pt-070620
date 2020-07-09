dccae0ba88ab103c5cc99bb5cc9abec08d7f0962
Jill Short <jillian.b.short@gmail.com>
2020-07-09 02:25:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma (phrase)
     'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == upcase
     "NO, NOT SINCE 1938!"
-  else phrase == downcase
+  else phrase == "Hi, Nana, how are you?"
     "HUH? SPEAK UP, SONNY!"
   end
 end
